ee4bfc5c1828603842049bd77a2ff8e1c93d92e6
Miguelgargor <140601367+Miguelgargor@users.noreply.github.com>
2023-07-26 13:19:01 +0200

    Update Miguel.py

--- Miguel.py
+++ Miguel.py
@@ -31,11 +31,11 @@ def funcion_prueba(peso=0.5, color_especial_arista='red', color_normal_arista='b
 
 def main():
     st.title('Aplicación de prueba')
-    st.markdown("<b>Peso</b>", unsafe_allow_html=True)
-    peso = st.slider('', min_value=0.0, max_value=1.0, value=0.5, step=0.01) # Step is float
+    #st.markdown("<b>Peso</b>", unsafe_allow_html=True)
+    peso = st.slider('**Peso**', min_value=0.0, max_value=1.0, value=0.5, step=0.01) # Step is float
     color_especial_arista = st.color_picker('Color especial arista', value='#00FFAA')
     color_normal_arista = st.color_picker('Color normal arista', value='#00FFAA')
-    tamaño_letra = st.slider('<b>Tamaño Fuente</b>', min_value=1, max_value=20, value=8, step=1) # Step is integer
+    tamaño_letra = st.slider('**Tamaño Fuente**', min_value=1, max_value=20, value=8, step=1) # Step is integer
     width = st.slider('Ancho figura', min_value=1.0, max_value=16.0, value=1.0, step=0.1) # Step is float
     height = st.slider('Alto figura', min_value=1.0, max_value=16.0, value=1.0, step=0.1) # Step is float
 
